dda11f4caf506c08ea1a90331ad38afdb6f552ec
muhammadali276397 <muhammadali246397@gmail.com>
2024-10-17 12:45:24 +0600

    make a loader animation with css

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,10 @@
-import { Form, FormSection, FormSubmit } from "../components/reusableForm";
-
+// import { Form, FormSection, FormSubmit } from "../components/reusableForm";
+import './About.css'
 
 const About = () => {
     return (
         <div>
-            <Form>
+            {/* <Form>
                 <FormSection>
                     <h1>this is formSection</h1>
                 </FormSection>
@@ -12,6 +12,20 @@ const About = () => {
                     <h1>this is form submit section</h1>
                 </FormSubmit>
             </Form>
+            <div className="border-2 box flex justify-center items-center">
+                <div className="box1"></div>
+            </div> */}
+
+                <div className='main'>
+                    <div className='loading'>
+                        <div className='circle'></div>
+                        <div className='square'></div>
+                    </div>
+
+                </div>
+
+
+
         </div>
     );
 };
